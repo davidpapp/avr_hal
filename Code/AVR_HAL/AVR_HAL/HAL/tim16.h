5333fb0484c9651c01ff15b57f4c98bae2ce658f
davidpapp <davidpapp@zoho.com>
2018-04-17 22:39:52 +0200

    Added TIM enums

--- Code/AVR_HAL/AVR_HAL/HAL/tim16.h
+++ Code/AVR_HAL/AVR_HAL/HAL/tim16.h
@@ -27,4 +27,50 @@ typedef struct
 #define Tim16_4 ((volatile TIM16_t*) 0xA0)
 #define Tim16_5 ((volatile TIM16_t*) 0x120)
 
+typedef enum
+{
+	WGM_NORMAL,
+	WGM_PWM_PC_8,
+	WGM_PWM_PC_9,
+	WGM_PWM_PC_10,
+	WGM_CTC_OCR,
+	WGM_FAST_PWM_8,
+	WGM_FAST_PWM_9,
+	WGM_FAST_PWM_10,
+	WGM_PWM_PFC_ICR,
+	WGM_PWM_PFC_OCRA,
+	WGM_PWM_PC_ICR,
+	WGM_PWM_PC_OCRA,
+	WGM_CTC_ICR,
+	WGM_FAST_PWM_ICR,
+	WGM_FAST_PWM_OCR
+}TIM_WGM_t;
+
+typedef enum
+{
+	COM_NORMAL,
+	COM_TOGGLE,
+	COM_CLEAR,
+	COM_SET
+}TIM_COM_t;
+
+typedef enum
+{
+	CS_NO_CLK,
+	CS_CLK_DIV1,
+	CS_CLK_DIV8,
+	CS_CLK_DIV64,
+	CS_CLK_DIV256,
+	CS_CLK_DIV1024,
+	CS_EXT_FALLING,
+	CS_EXT_RISING
+}TIM_CS_t;
+
+typedef enum
+{
+	OCR_A,
+	OCR_B,
+	OCR_C
+}TIM_OCR_t;
+
 #endif /* TIM16_H_ */
--- Code/AVR_HAL/AVR_HAL/main.c
+++ Code/AVR_HAL/AVR_HAL/main.c
@@ -7,13 +7,28 @@
 
 #include <avr/io.h>
 #include "HAL/tim16.h"
+#include "HAL/Gpio.h"
 
 int main(void)
 {
-	Tim16_1->ICR = 0x0000;
+	GPIOL->DDR = 0xFF;
+
+	Tim16_5->ICR = 0x9C3F;
+	Tim16_5->OCRA = 0x0000;
+	Tim16_5->OCRB = 0x0BB7;
+	Tim16_5->OCRC = 0x0000;
+	
+	Tim16_5->TCCRC = 0x00;
+	Tim16_5->TCCRB = 0b00011010;
+	Tim16_5->TCCRA = 0b00110010;
+
+	Tim16_5->TCCRA = 0x00;
+	Tim16_5->OCRB = 0x00B7;
+	Tim16_5->TCCRA = 0b00110010;
     /* Replace with your application code */
     while (1) 
     {
+		
     }
 }
 
